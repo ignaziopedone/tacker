b36e008f4b9d64ce4bebd74026b3bb5b4127caea
Ignazio Pedone <i.pedone@live.it>
2018-10-24 15:51:52 +0200

    First test adding files and changing scripts.

--- configurazione_openstack/OpenstackConf.sh
+++ configurazione_openstack/OpenstackConf.sh
@@ -4,4 +4,4 @@ sudo ip link set br-ex up
 sudo ip route add 172.24.4.0/24 dev br-ex
 sudo ip addr add 172.24.4.1/24 dev br-ex
 sudo iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE #change the network interface
-sudo route add -net 10.208.0.0/24 gw 172.24.4.4 #change this number with the router created by openstack script configuration
+sudo route add -net 10.208.0.0/24 gw 172.24.4.13 #change this number with the router created by openstack script configuration
