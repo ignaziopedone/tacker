752e33a8f574fe5312439cba1a4be5955b52a5fb
Ignazio Pedone <ignazio.pedone@gmail.com>
2018-10-31 09:45:44 +0100

    Changing python formatting in line with autopep8.

--- vnsf_config/ansible/l3filter/l3_filter_vnsfm.py
+++ vnsf_config/ansible/l3filter/l3_filter_vnsfm.py
@@ -4,10 +4,13 @@ from subprocess import Popen, PIPE
 # packets_list = "nmap"
 # result = subprocess.call(["ansible-playbook", playbook, "--extra-vars", "ansible_sudo_pass=quantum packets={}".format(packets_list)])
 # the format of extra_vars should be "ansible_sudo_pass=quantum packets={}".format(packets_list)
+
+
 def exec_ansible_playbook(filename, extra_vars):
     try:
         print "[RUN] {} playbook".format(filename)
-        result = subprocess.Popen(["ansible-playbook", filename, "--extra-vars", extra_vars], stdout=PIPE)
+        result = subprocess.Popen(
+            ["ansible-playbook", filename, "--extra-vars", extra_vars], stdout=PIPE)
         output, err = result.communicate()
         logging.info(output)
     except Exception as e:
@@ -16,7 +19,8 @@ def exec_ansible_playbook(filename, extra_vars):
 
 def set_policies_from_file(src_mspl, target_vnsf):
     try:
-        exec_ansible_playbook("set_policies_from_file.yml", "target={} src_file={}".format(target_vnsf, src_mspl))
+        exec_ansible_playbook("set_policies_from_file.yml",
+                              "target={} src_file={}".format(target_vnsf, src_mspl))
     except Exception as e:
         print e
 
